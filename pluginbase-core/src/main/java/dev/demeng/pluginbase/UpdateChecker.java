d2a895df9d9ce9f3c934ac0933963a8d932b6680
Demeng7215 <Demeng7215Official@gmail.com>
2021-04-03 23:45:23 -0400

    Add resource update checker

---
+++ pluginbase-core/src/main/java/dev/demeng/pluginbase/UpdateChecker.java
@@ -0,0 +1,79 @@
+package dev.demeng.pluginbase;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Simple utility for checking for resource updates on SpigotMC.
+ */
+@SuppressWarnings("unused")
+public class UpdateChecker {
+
+  private UpdateChecker() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  /**
+   * Gets the latest version of the resource with the specified resource ID.
+   *
+   * @param resourceId The ID of the resource to check
+   * @return The latest version string, or null if failed to retrieve version
+   */
+  public static String getLatestVersion(int resourceId) {
+
+    try (InputStream inputStream = new URL(
+        "https://api.spigotmc.org/legacy/update.php?resource=" + resourceId).openStream();
+        Scanner scanner = new Scanner(inputStream)) {
+
+      if (scanner.hasNext()) {
+        return scanner.next();
+      }
+
+    } catch (IOException ex) {
+      return null;
+    }
+
+    return null;
+  }
+
+  /**
+   * Checks if this plugin version is the same as the one with the specified resource ID.
+   *
+   * @param resourceId The ID of the resource to check
+   * @return The update check result
+   */
+  public static UpdateChecker.Result isUpToDate(int resourceId) {
+
+    final String latest = getLatestVersion(resourceId);
+
+    if (latest == null) {
+      return UpdateChecker.Result.ERROR;
+    }
+
+    if (Common.getVersion().equals(latest)) {
+      return UpdateChecker.Result.UP_TO_DATE;
+    }
+
+    return UpdateChecker.Result.OUTDATED;
+  }
+
+  /**
+   * The result of an update check.
+   */
+  public enum Result {
+    /**
+     * The plugin version matches the one on the resource.
+     */
+    UP_TO_DATE,
+    /**
+     * The plugin version does not match the one on the resource.
+     */
+    OUTDATED,
+    /**
+     * There was an error whilst checking for updates.
+     */
+    ERROR
+  }
+}
