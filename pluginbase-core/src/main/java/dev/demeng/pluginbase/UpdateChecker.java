258541a7597b38cc67963df292663d555e52374e
Demeng7215 <Demeng7215Official@gmail.com>
2021-04-07 10:01:46 -0400

    Fix license header

--- pluginbase-core/src/main/java/dev/demeng/pluginbase/UpdateChecker.java
+++ pluginbase-core/src/main/java/dev/demeng/pluginbase/UpdateChecker.java
@@ -2,7 +2,6 @@
  * MIT License
  *
  * Copyright (c) 2021 Demeng Chen
- * Copyright (c) contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
