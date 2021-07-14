53c6a4674d79a46603396714ba7165d37d2c4725
Demeng7215 <Demeng7215Official@gmail.com>
2021-07-14 11:24:46 -0400

    Create placeholders utility

---
+++ pluginbase-core/src/main/java/dev/demeng/pluginbase/chat/Placeholders.java
@@ -0,0 +1,90 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Demeng Chen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.demeng.pluginbase.chat;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a series of placeholders for a message.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Placeholders {
+
+  /**
+   * A map of all the placeholders, with the key being the string to replace and the value being the
+   * string to replace with.
+   */
+  @NotNull @Getter private final Map<String, String> replacements = new HashMap<>();
+
+  /**
+   * Creates a new placeholders object with the given placeholder.
+   *
+   * @param toReplace   The string to replace
+   * @param replaceWith The string to replace with
+   * @return The placeholders object
+   */
+  @NotNull
+  public static Placeholders of(@NotNull String toReplace, @NotNull String replaceWith) {
+    final Placeholders obj = new Placeholders();
+    obj.getReplacements().put(toReplace, replaceWith);
+    return obj;
+  }
+
+  /**
+   * Adds a new placeholder to hte placeholders object.
+   *
+   * @param toReplace   The string to replace
+   * @param replaceWith The string to replace with
+   * @return this
+   */
+  @NotNull
+  public Placeholders add(@NotNull String toReplace, @NotNull String replaceWith) {
+    replacements.put(toReplace, replaceWith);
+    return this;
+  }
+
+  /**
+   * Sets the placeholders into the given string.
+   *
+   * @param str The string to have placeholders set
+   * @return The replaced string
+   */
+  @NotNull
+  public String set(@NotNull String str) {
+
+    String replaced = str;
+
+    for (Map.Entry<String, String> entry : replacements.entrySet()) {
+      replaced = replaced.replace(entry.getKey(), entry.getValue());
+    }
+
+    return replaced;
+  }
+}
