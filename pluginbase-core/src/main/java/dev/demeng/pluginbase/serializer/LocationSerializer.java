55f1236e5a9c40b7bd8e4a0419c0cd52066b200c
Demeng7215 <Demeng7215Official@gmail.com>
2021-07-16 12:08:55 -0400

    Create location serializer

---
+++ pluginbase-core/src/main/java/dev/demeng/pluginbase/serializer/LocationSerializer.java
@@ -0,0 +1,116 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Demeng Chen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.demeng.pluginbase.serializer;
+
+import dev.demeng.pluginbase.Common;
+import dev.demeng.pluginbase.ServerProperties;
+import dev.demeng.pluginbase.YamlConfig;
+import dev.demeng.pluginbase.serializer.interfaces.YamlSerializable;
+import java.io.IOException;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The serializer for {@link Location}.
+ */
+public class LocationSerializer implements YamlSerializable<Location> {
+
+  @Override
+  public void serialize(@NotNull Location obj, @NotNull YamlConfig configFile, @NotNull String path)
+      throws IOException {
+
+    if (obj.getWorld() != null) {
+      configFile.getConfig().set(path + ".world", obj.getWorld().getName());
+    }
+
+    serializeNoWorld(obj, configFile, path);
+  }
+
+  /**
+   * Serializes a location without the world name. When deserialized, it will attempt to get the
+   * default world from server.properties.
+   */
+  public void serializeNoWorld(
+      @NotNull Location obj,
+      @NotNull YamlConfig configFile,
+      @NotNull String path
+  ) throws IOException {
+
+    configFile.getConfig().set(path + ".x", obj.getX());
+    configFile.getConfig().set(path + ".y", obj.getY());
+    configFile.getConfig().set(path + ".z", obj.getZ());
+    configFile.getConfig().set(path + ".yaw", obj.getYaw());
+    configFile.getConfig().set(path + ".pitch", obj.getPitch());
+
+    configFile.save();
+  }
+
+  /**
+   * Serializes a location without the yaw or pitch. When deserialized, the yaw and pitch will be
+   * 0.
+   */
+  public void serializeNoRotation(
+      @NotNull Location obj,
+      @NotNull YamlConfig configFile,
+      @NotNull String path
+  ) throws IOException {
+
+    if (obj.getWorld() != null) {
+      configFile.getConfig().set(path + ".world", obj.getWorld().getName());
+    }
+
+    configFile.getConfig().set(path + ".x", obj.getX());
+    configFile.getConfig().set(path + ".y", obj.getY());
+    configFile.getConfig().set(path + ".z", obj.getZ());
+
+    configFile.save();
+  }
+
+  @Override
+  public Location deserialize(@NotNull ConfigurationSection section) {
+
+    String worldName = section.getString("world");
+
+    if (worldName == null) {
+      worldName = Common.getOrDefault(ServerProperties.getProperty("level-name"), "world");
+    }
+
+    final World world = Bukkit.getWorld(worldName);
+
+    if (world == null) {
+      throw new IllegalArgumentException("World '" + worldName + "' is not loaded");
+    }
+
+    return new Location(world,
+        section.getDouble("x"),
+        section.getDouble("y"),
+        section.getDouble("z"),
+        (float) section.getDouble("yaw"),
+        (float) section.getDouble("pitch"));
+  }
+}
