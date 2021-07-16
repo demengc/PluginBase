4f2aeb3271f547a135913d7b853e247e3c69a9f0
Demeng7215 <Demeng7215Official@gmail.com>
2021-07-16 12:33:16 -0400

    Create LocationUtils

---
+++ pluginbase-core/src/main/java/dev/demeng/pluginbase/LocationUtils.java
@@ -0,0 +1,61 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Demeng Chen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.demeng.pluginbase;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.bukkit.Location;
+
+/**
+ * Utilities for {@link org.bukkit.Location}.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class LocationUtils {
+
+  /**
+   * Rounds a location to the center of a block.
+   *
+   * @param loc The location to center
+   * @return The centered location
+   */
+  public static Location center(Location loc) {
+    return new Location(loc.getWorld(),
+        loc.getBlockX() + 0.5,
+        loc.getBlockY() + 0.5,
+        loc.getBlockZ() + 0.5,
+        loc.getYaw(),
+        loc.getPitch());
+  }
+
+  /**
+   * Converts a location to a block location (world and integer coordinates).
+   *
+   * @param loc The location to convert to a block location
+   * @return The block position
+   */
+  public static Location toBlockLocation(Location loc) {
+    return new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+  }
+}
