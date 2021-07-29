2f9cda530a46c90963a30c94629ab1846d629a35
Demeng7215 <Demeng7215Official@gmail.com>
2021-07-29 12:12:58 -0400

    Fix center util modifying Y axis, add nullability annotations

--- pluginbase-core/src/main/java/dev/demeng/pluginbase/LocationUtils.java
+++ pluginbase-core/src/main/java/dev/demeng/pluginbase/LocationUtils.java
@@ -27,6 +27,7 @@ package dev.demeng.pluginbase;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.bukkit.Location;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Utilities for {@link org.bukkit.Location}.
@@ -40,10 +41,11 @@ public final class LocationUtils {
    * @param loc The location to center
    * @return The centered location
    */
-  public static Location center(final Location loc) {
+  @NotNull
+  public static Location center(@NotNull final Location loc) {
     return new Location(loc.getWorld(),
         loc.getBlockX() + 0.5,
-        loc.getBlockY() + 0.5,
+        loc.getBlockY(),
         loc.getBlockZ() + 0.5,
         loc.getYaw(),
         loc.getPitch());
@@ -55,7 +57,8 @@ public final class LocationUtils {
    * @param loc The location to convert to a block location
    * @return The block position
    */
-  public static Location toBlockLocation(final Location loc) {
+  @NotNull
+  public static Location toBlockLocation(@NotNull final Location loc) {
     return new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
   }
 }
