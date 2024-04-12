98c1dd025ace0c36c037d72916ec46d47a201d3f
Demeng7215 <demengc6@gmail.com>
2024-04-12 15:10:55 -0400

    Add Placeholders

---
+++ pluginbase-core/src/main/java/dev/demeng/pluginbase/model/Placeholders.java
@@ -0,0 +1,21 @@
+package dev.demeng.pluginbase.model;
+
+import dev.demeng.pluginbase.DynamicPlaceholders;
+import java.util.function.UnaryOperator;
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A shorthand version of {@link DynamicPlaceholders}, allowing for string placeholders to be easily
+ * applied on strings, lists, and item stacks.
+ */
+@Data(staticConstructor = "of")
+public class Placeholders implements DynamicPlaceholders {
+
+  private final UnaryOperator<String> placeholderReplacer;
+
+  @Override
+  public @NotNull String setPlaceholders(@NotNull String str) {
+    return placeholderReplacer.apply(str);
+  }
+}
